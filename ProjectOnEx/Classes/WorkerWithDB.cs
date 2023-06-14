300d76b2915c99a4ad1e43850d7b6c093e75380e
3-306-5 <SteaMMe123@ya.ru>
2023-06-14 12:09:10 +0700

    add new method

--- ProjectOnEx/Classes/WorkerWithDB.cs
+++ ProjectOnEx/Classes/WorkerWithDB.cs
@@ -77,18 +77,27 @@ namespace ProjectOnEx.Classes
             List<Markers> markers = new List<Markers>();
             if (ConnectOpen())
             {
-                using (SqlCommand cmd = new SqlCommand($"НАПИСАТЬ СКРИПТ", connection))
+                int mount = int.Parse(Console.ReadLine());
+                int enddate = mount + 1;
+                int year = 2001;
+                int endyear = year;
+                if (mount == 12)
+                {
+                    enddate = 1;
+                    endyear += 1;
+                }
+                connection.Open();
+                using (SqlCommand cmd = new SqlCommand($"DECLARE " +
+                    $"@startdate datetime = '01-{mount}-{year}', " +
+                    $"@enddate datetime = '01-{enddate}-{endyear}' " +
+                    "SELECT * FROM dates " +
+                    "WHERE time >= @startdate AND time < @enddate", connection))
                 {
                     var reader = cmd.ExecuteReader();
                     while (reader.Read())
                     {
-                        markers.Add(new Markers
-                        {
-                            Student = (string)reader.GetValue(0) + " " + (string)reader.GetValue(1), // объедтнение имени и фамилии
-                            Room = (string)reader.GetValue(2),
-                            Mark = (char)reader.GetValue(3),
-                            Day = (int)reader.GetValue(4).
-                        });
+                        Console.WriteLine(reader.GetValue(0).ToString());
+                        Console.WriteLine("Day " + DateTime.Parse(reader.GetValue(0).ToString()).Day.ToString());
                     }
                 }
                 ConnectClose();
